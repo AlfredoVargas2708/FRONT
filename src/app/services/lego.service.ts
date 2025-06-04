28032331e2949e35204dee80d5ff8ebdbc53cfcc
alfredo-vargas <alfvargas@alumnos.uai.cl>
2025-06-04 13:08:10 -0400

    Add environment configuration for production and development APIs

--- src/app/services/lego.service.ts
+++ src/app/services/lego.service.ts
@@ -1,33 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LegoService {
-  private apiUrlCloud = 'https://back-anlk.onrender.com/lego'; // Replace with your actual API URL
-  private apiUrl = 'http://localhost:3000/lego'; // Local API URL for development
 
   constructor(private http: HttpClient) { }
 
   getAllLegoPieces(): Observable<any> {
-    return this.http.get<any>(this.apiUrlCloud);
+    return this.http.get<any>(`${environment.apiUrl}`);
   }
 
   getLegoPieceByCode(code: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrlCloud}/${code}`);
+    return this.http.get<any>(`${environment.apiUrl}/${code}`);
   }
 
   editLegoPieceInBBDD(id: number, legoPiece: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrlCloud}/${id}`, legoPiece);
+    return this.http.put<any>(`${environment.apiUrl}/${id}`, legoPiece);
   }
 
   addLegoPieceToBBDD(legoPiece: any): Observable<any> {
-    return this.http.post<any>(this.apiUrlCloud, legoPiece);
+    return this.http.post<any>(`${environment.apiUrl}`, legoPiece);
   }
 
   deleteLegoPieceFromBBDD(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrlCloud}/${id}`);
+    return this.http.delete<any>(`${environment.apiUrl}/${id}`);
   }
 }
